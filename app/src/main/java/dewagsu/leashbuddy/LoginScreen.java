dd97b6fb8606162b6baa179a04b5f26c2c467a9e
william-crawford <will.crawford@live.com>
2017-11-04 15:24:53 -0400

    Login screen goes to owner_home

--- app/src/main/java/dewagsu/leashbuddy/LoginScreen.java
+++ app/src/main/java/dewagsu/leashbuddy/LoginScreen.java
@@ -1,7 +1,10 @@
 package dewagsu.leashbuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginScreen extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        Button ownerLogin = (Button) findViewById(R.id.owner_login_btn);
+        ownerLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToOwnerHome();
+            }
+        });
+        Button walkerLogin = (Button) findViewById(R.id.walker_login_btn);
+        Button signup = (Button) findViewById(R.id.signup_btn);
     }
+
+    private void goToOwnerHome() {
+        Intent intent = new Intent(this, OwnerHome.class);
+        startActivity(intent);
+    }
+
 }
